c64c377256e3b2749f5f98ef6f41224ca298c1a9
Franck Nijhof <git@frenck.dev>
2022-06-12 22:18:48 +0200

    ⬆️ Upgrades add-on base image to 12.0.0 (#46)

--- prometheus/rootfs/etc/cont-init.d/prometheus.sh
+++ prometheus/rootfs/etc/cont-init.d/prometheus.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: Prometheus
 # Configures Prometheus
 # ==============================================================================
 echo "${SUPERVISOR_TOKEN}" > '/run/home-assistant.token'
+
+mkdir -p /data/prometheus
+find /data/prometheus -not -perm 0644 -type f -exec chmod 0660 {} \;
+find /data/prometheus -not -perm 0755 -type d -exec chmod 0770 {} \;
