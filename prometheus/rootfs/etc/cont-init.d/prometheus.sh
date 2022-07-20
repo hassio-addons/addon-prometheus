703506d30f29a32bd503f354dcf09dc6075295eb
Nathan Ellsworth <35043496+42Network@users.noreply.github.com>
2022-07-20 02:32:55 -0500

    Update prometheus.sh

--- prometheus/rootfs/etc/cont-init.d/prometheus.sh
+++ prometheus/rootfs/etc/cont-init.d/prometheus.sh
@@ -6,6 +6,6 @@
 echo "${SUPERVISOR_TOKEN}" > '/run/home-assistant.token'
 
 mkdir -p /data/prometheus
-chown prometheus:prometheus /data/prometheus
+chown -R prometheus:prometheus /data/prometheus
 find /data/prometheus -not -perm 0644 -type f -exec chmod 0660 {} \;
 find /data/prometheus -not -perm 0755 -type d -exec chmod 0770 {} \;
